6b48e437d6c959f0e7ae846a9d8dde551b896463
zgh1949 <zgh1949@outlook.com>
2022-03-08 12:46:25 +0800

    init

---
+++ effective-java/src/main/java/zgh1949/space/effectivejava/EffectiveJavaApplication.java
@@ -0,0 +1,13 @@
+package zgh1949.space.effectivejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveJavaApplication.class, args);
+    }
+
+}
---
+++ effective-java/src/test/java/zgh1949/space/effectivejava/EffectiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package zgh1949.space.effectivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
